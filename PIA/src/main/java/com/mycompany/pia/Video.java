a46df3d224e6e7aa6e32b97488fc1306add03eff
tetraphoz <tetraphosphorus@gmail.com>
2022-12-05 02:57:06 -0600

    Agregar clase Video

---
+++ PIA/src/main/java/com/mycompany/pia/Video.java
@@ -0,0 +1,10 @@
+package com.mycompany.pia;
+
+import java.nio.file.Path;
+
+public class Video {
+    public String name;
+    public String artist;
+    public Path videoPath;
+    public Path imagePath;
+}
