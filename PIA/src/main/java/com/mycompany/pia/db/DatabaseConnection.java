b4aedd0c64196b8cdfd5a8f70bd05619571618d8
tetraphoz <tetraphosphorus@gmail.com>
2022-12-05 05:30:49 -0600

    Arreglar conexion a base de datos

--- PIA/src/main/java/com/mycompany/pia/db/DatabaseConnection.java
+++ PIA/src/main/java/com/mycompany/pia/db/DatabaseConnection.java
@@ -1,6 +1,5 @@
 package com.mycompany.pia.db;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,18 +11,18 @@ public class DatabaseConnection {
   private Connection connection;
 
   public DatabaseConnection() throws IOException, SQLException {
-    // Cargar la configuarcion
-    Properties config = new Properties();
-    config.load(new FileInputStream("config.properties"));
-
-    // Obtener las propiedades
-    String url = config.getProperty("database.url");
-    String db_name = config.getProperty("database.name");
-    String username = config.getProperty("database.username");
-    String password = config.getProperty("database.password");
-
-    // Crear la conexion
-    this.connection = DriverManager.getConnection(url+db_name, username, password);
+	  // Cargar la configuarcion
+	  Properties config = new Properties();
+	  config.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+	  
+	  // Obtener las propiedades
+	  String url = config.getProperty("database.url");
+	  String db_name = config.getProperty("database.db_name");
+	  String username = config.getProperty("database.username");
+	  String password = config.getProperty("database.password");
+	  
+	  // Crear la conexion
+	  this.connection = DriverManager.getConnection(url+"/"+db_name, username, password);
   }
 
   public Connection getConnection() {
