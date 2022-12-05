fe81f99499afea21c6b75e87e116318a6b5fdd2f
tetraphoz <tetraphosphorus@gmail.com>
2022-12-05 03:54:47 -0600

    Hacer los campos de Video privados

--- PIA/src/main/java/com/mycompany/pia/Video.java
+++ PIA/src/main/java/com/mycompany/pia/Video.java
@@ -3,8 +3,40 @@ package com.mycompany.pia;
 import java.nio.file.Path;
 
 public class Video {
-    public String name;
-    public String artist;
-    public Path videoPath;
-    public Path imagePath;
+	private String name;
+	private String artist;
+	private Path videoPath;
+	private Path imagePath;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	
+	public Path getVideoPath() {
+		return videoPath;
+	}
+	
+	public void setVideoPath(Path videoPath) {
+		this.videoPath = videoPath;
+	}
+	
+	public Path getImagePath() {
+		return imagePath;
+	}
+	
+	public void setImagePath(Path imagePath) {
+		this.imagePath = imagePath;
+	}
 }
