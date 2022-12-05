89a37124e3cec19b5ff1b423879b61b3285659a2
tetraphoz <tetraphosphorus@gmail.com>
2022-12-05 04:12:24 -0600

    Agregar campo idVideo

--- PIA/src/main/java/com/mycompany/pia/Video.java
+++ PIA/src/main/java/com/mycompany/pia/Video.java
@@ -3,16 +3,26 @@ package com.mycompany.pia;
 import java.nio.file.Path;
 
 public class Video {
+	private int idVideo;
 	private String name;
 	private String artist;
 	private Path videoPath;
 	private Path imagePath;
 
-	public Video(String name, String artist, Path videoPath, Path imagePath){
+	public Video(int idVideo, String name, String artist, Path videoPath, Path imagePath){
+		this.idVideo = idVideo;
 		this.name = name;
 		this.artist = artist;
 		this.videoPath = videoPath;
 		this.imagePath = imagePath;
+	}
+
+	public int getIdVideo() {
+		return idVideo;
+	}
+
+	public void setIdVideo(int idVideo) {
+		this.idVideo = idVideo;
 	}
 	
 	public String getName() {
