d60de33d1c6e8ecd193bd41615da1a40af8c4c0f
tetraphoz <tetraphosphorus@gmail.com>
2022-12-05 02:52:18 -0600

    Agregar conector a base de datos que usa el config

---
+++ PIA/src/main/java/com/mycompany/pia/DatabaseConnection.java
@@ -0,0 +1,32 @@
+package com.mycompany.pia;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnection {
+
+  private Connection connection;
+
+  public DatabaseConnection() throws IOException, SQLException {
+    // Cargar la configuarcion
+    Properties config = new Properties();
+    config.load(new FileInputStream("config.properties"));
+
+    // Obtener las propiedades
+    String url = config.getProperty("database.url");
+    String db_name = config.getProperty("database.name");
+    String username = config.getProperty("database.username");
+    String password = config.getProperty("database.password");
+
+    // Crear la conexion
+    this.connection = DriverManager.getConnection(url+db_name, username, password);
+  }
+
+  public Connection getConnection() {
+    return this.connection;
+  }
+}
